e4663c711ecdc34fedd40a3f4c77b723bcf3dbf2
kirikkk92 <ykrop1992@mail.ru>
2020-01-10 21:15:56 +0300

    add12

--- lesson12/src/com/company/Main.java
+++ lesson12/src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// дженерики
+        User<String, String> u = new User<>();
+        User<Date, Object> u2 = new User<>();
+        u.id = "SOME ID";
+        System.out.println(u.id.toLowerCase());
+        u2.id = new Date();
+        System.out.println(u2.id.getTime());
+        //
+        Car<User, String> car = new Car<>();
+
+        //коллекции
+        //ArrayList (все элементы располагаются друг за другом,
+        //порядок элементов не меняется, возможен случайный доступ к элементам)
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hello");// добавляет элемент всегда в конец списка
+        list.add("world");
+        list.add(1,"Java");// добавляет элемент в выбранный индекс
+        for (int i = 0; i < list.size();i++) {
+            System.out.println(list.get(i));
+        }
+        list.remove("hello");
+        System.out.println(list);
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(10);
+        list2.add(20);
+        list2.add(30);
+        list2.remove(Integer.valueOf(20));// или Integer i = 20;
+        System.out.println(list2);
+
+
     }
 }
