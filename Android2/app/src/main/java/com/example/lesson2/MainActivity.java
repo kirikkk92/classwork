ea770f84f1be1b54d9c5fe03de93c721298022b7
kirikkk92 <ykrop1992@mail.ru>
2020-02-28 21:01:47 +0300

    andr

--- Android2/app/src/main/java/com/example/lesson2/MainActivity.java
+++ Android2/app/src/main/java/com/example/lesson2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.lesson2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText login = findViewById(R.id.login);
+        final EditText password = findViewById(R.id.password);
+        Button loginBtn = findViewById(R.id.loginBtn);
+        final TextView errorMsg = findViewById(R.id.errorMsg);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String error = "";
+                if (login.getText().toString().equals("")){
+                    error = "Заполните поле \"Логин\"\n";
+                }
+                if (password.getText().toString().equals("")){
+                    error += "Заполните поле \"Пароль\"\n";
+                }
+                if (!error.equals("")){
+                    errorMsg.setText(error);
+                    errorMsg.setVisibility(View.VISIBLE);
+                    return;
+                }
+            }
+        });
     }
 }
