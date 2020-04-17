436740e8ed0e7fb07adb7261812b2d1d93323c63
kirikkk92 <ykrop1992@mail.ru>
2020-04-17 21:00:30 +0300

    add HTML

---
+++ 07.04.2020/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int m [][] = new int [3][5];//3 строки,5 столбцы
+	    m[0][0]=12;
+	    m[2][4]=-1;
+
+	    
+    }
+}
